57aa408499a5c24f7b4161f4c82eedcf0dcc06ae
witwint <witwint1234@gmail.com>
2023-12-06 14:14:34 +0900

    초기 셋팅

---
+++ server/src/test/java/kirukiru/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kirukiru.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
